18408f84e775ceb5c7b06383767ae46f0ad62c74
Xiaoke Fei <bubu@xiaokes-mbp.myfiosgateway.com>
2024-10-02 18:53:30 -0400

    .py code for standard input and output

---
+++ scr/standard_input_output.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+
+#the input() function returns a string
+gpa_string = input("What is your GPA?")
+gpa = float(gpa_string)
+
+print("Name: ", name)
+print("GPA: ", gpa)
