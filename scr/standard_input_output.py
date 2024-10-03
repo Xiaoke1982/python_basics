4fc062fd5628e6738872da57628f52f7be1f75af
Xiaoke Fei <bubu@xiaokes-mbp.myfiosgateway.com>
2024-10-03 10:23:13 -0400

    update formatted output

--- scr/standard_input_output.py
+++ scr/standard_input_output.py
@@ -4,5 +4,16 @@ name = input("What is your name?")
 gpa_string = input("What is your GPA?")
 gpa = float(gpa_string)
 
+exam1_str = input("What is your exam1 score?")
+exam2_str = input("What is your exam2 score?")
+exam3_str = input("What is your exam3 score?")
+
+exam1 = float(exam1_str)
+exam2 = float(exam2_str)
+exam3 = float(exam3_str)
+
+avg = (exam1 + exam2 + exam3) / 3.0
+
 print("Name: ", name)
 print("GPA: ", gpa)
+print("Your average score: %5.2f" %avg)
